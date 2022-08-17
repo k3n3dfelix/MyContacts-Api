9675980f0dced27b3931e0fed68970f6d3ed15de
Kened Silva Felix <ksfelix1@stefanini.com>
2022-08-17 08:46:47 -0300

    Configurando Cors

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
+app.use((request, response,  next) =>{
+  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+})
 app.use(routes);
 
 app.listen(3000, () => console.log('server started'));
